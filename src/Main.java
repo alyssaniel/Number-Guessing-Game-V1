7fce5e4fab73b2826310e4cd933e77f6e8f4a262
Alyssa <alyssa.nichols.2022@student.lhschools.org>
2019-03-11 08:15:00 -0400

    Added The If and Else If's

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,12 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Scanner keyboard;
-        int pickednumber;
+        int pickedNumber;
         int go = 0;
         keyboard= new Scanner(System.in);
         Random generator = new Random();
 
+        int numberOfTries = 0;
 
 
 
@@ -21,16 +22,49 @@ public class Main {
         System.out.println("Hola! Pick a number between 1 and 1000 to win!");
         while (go == 0) {
 
+            System.out.println("Pick a number between 1-1000");
+            pickedNumber = keyboard.nextInt();
 
             int numberToWin = generator.nextInt(1000);
             System.out.println(numberToWin);
             boolean win = false;
-            System.out.println("Pick a number between 1-1000");
-            pickednumber = keyboard.nextInt();
+
+
+            numberOfTries++;
+            while (!win) {
+
+
+                if (pickedNumber == numberToWin) {
+                    win = true;
+
+
+                } else if (pickedNumber < numberToWin) {
+                    System.out.println("The number you have chosen is too low. Guess again:");
+                    pickedNumber = keyboard.nextInt();
+                } else if (pickedNumber > numberToWin) {
+                    System.out.println("The number you have chosen is too high. Guess again:");
+                    pickedNumber = keyboard.nextInt();
+                }
+              }
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
         }
 
 
 
 
     }
-}
+
