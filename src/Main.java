7a7bba2d8d40b7ba88589117f2f6c84f66bf7ca5
Alyssa <alyssa.nichols.2022@student.lhschools.org>
2019-03-14 07:51:03 -0400

    Added A Loop

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,19 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+
         Scanner keyboard;
+        //int's
         int pickedNumber;
         int go = 0;
-        keyboard= new Scanner(System.in);
-        Random generator = new Random();
-
         int numberOfTries = 0;
+        keyboard = new Scanner(System.in);
+        Random generator = new Random();
+        String keepGoing;
 
-
-
-
+        Scanner continueKB;
+        continueKB = new Scanner(System.in);
 
 
         System.out.println("Hola! Pick a number between 1 and 1000 to win!");
@@ -26,7 +27,7 @@ public class Main {
             pickedNumber = keyboard.nextInt();
 
             int numberToWin = generator.nextInt(1000);
-            System.out.println(numberToWin);
+
             boolean win = false;
 
 
@@ -36,6 +37,8 @@ public class Main {
 
                 if (pickedNumber == numberToWin) {
                     win = true;
+                    System.out.println(numberToWin);
+                    System.out.println();
 
 
                 } else if (pickedNumber < numberToWin) {
@@ -45,26 +48,20 @@ public class Main {
                     System.out.println("The number you have chosen is too high. Guess again:");
                     pickedNumber = keyboard.nextInt();
                 }
-              }
-
-
-
-
             }
 
 
-
-
-
-
-
-
+            System.out.println("You won! The number you have chosen, " + pickedNumber + " was the right number! ");
+            System.out.println("Would you like to play again?");
+            System.out.println("Type 'y' for yes or 'n' for no.");
+            keepGoing = continueKB.nextLine();
+            go = keepGoing.indexOf("y");
 
 
         }
 
 
-
-
     }
+
+}
 
